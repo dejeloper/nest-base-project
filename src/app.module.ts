040cb7c39e16ea3586e24871618a6065113c5c38
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-07-27 01:09:41 -0500

    feat: add Role entity with id, name, createdAt, and updatedAt fields

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import {PrismaModule} from 'prisma/prisma.module';
 import {AuthModule} from '@/auth/auth.module';
 import {UserModule} from '@/user/user.module';
 import {RoleModule} from '@/role/role.module';
+import {PermissionsModule} from '@/permissions/permissions.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, RoleModule],
+  imports: [PrismaModule, UserModule, AuthModule, RoleModule, PermissionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
