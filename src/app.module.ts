7f1cebee261dcc5095ed5fcd75c55ed3a0f9bb70
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-08-10 23:14:03 -0500

    feat: add ConfigModule to AppModule for global configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 
@@ -8,9 +9,12 @@ import {UserModule} from '@/user/user.module';
 import {RoleModule} from '@/core/role/role.module';
 import {PermissionsModule} from '@/core/permissions/permissions.module';
 import {UserScheduleModule} from '@/core/user-schedule/user-schedule.module';
+import {PaymentModule} from './core/payments/infrastructure/payment.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, RoleModule, PermissionsModule, UserScheduleModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true, cache: true}),
+    PrismaModule, UserModule, AuthModule, RoleModule, PermissionsModule, UserScheduleModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
